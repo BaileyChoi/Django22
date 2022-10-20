b90a67250c7a871d9ac43dc4f2d345426745109c
BingBong <bingbong9981@gmail.com>
2022-10-20 14:33:00 +0900

    포스트 목록/상세 페이지에 author 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,12 +1,15 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class TestView(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.user_kim = User.objects.create_user(username="kim", password="somepassword")
+        self.user_lee = User.objects.create_user(username="lee", password="somepassword")
 
     def nav_test(self, soup):
         navbar = soup.nav
@@ -36,8 +39,10 @@ class TestView(TestCase):
         main_area = soup.find('div', id="main-area")
         self.assertIn('아무 게시물이 없습니다.', main_area.text)
         # 2. Post가 있는 경우
-        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.")
-        post_002 = Post.objects.create(title="두번째 포스트", content="두번째 포스트입니다.")
+        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.",
+                                       author=self.user_kim)
+        post_002 = Post.objects.create(title="두번째 포스트", content="두번째 포스트입니다.",
+                                       author=self.user_lee)
         self.assertEqual(Post.objects.count(), 2)
 
         # 포스트 목록 페이지를 새로고침 했을 때
@@ -49,13 +54,19 @@ class TestView(TestCase):
         main_area = soup.find('div', id="main-area")
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
+
+        # main area에 포스트 2개 작성자가 있는지
+        self.assertIn(post_001.author.username.upper(), main_area.text)
+        self.assertIn(post_002.author.username.upper(), main_area.text)
+
         # '아직 게시물이 없습니다'가 더이상 나타나지 않는지
         self.assertNotIn('아무 게시물이 없습니다.', main_area.text)
 
 
     def test_post_detail(self):
         # 상세 페이지가 있으려면 적어도 포스트 하나가 필요함
-        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.")
+        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.",
+                                       author=self.user_kim)
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
 
         # 포스트 상세 페이지 가져오기
@@ -77,6 +88,7 @@ class TestView(TestCase):
         post_area = main_area.find('div', id='post-area')
         self.assertIn(post_001.title, post_area.text)
         self.assertIn(post_001.content, post_area.text)
+        self.assertIn(post_001.author.username.upper(), post_area.text)
 
 
 
