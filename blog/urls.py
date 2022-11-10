6cb96d32ed82e73c50a2408bcd2e3e3c102fc962
BingBong <bingbong9981@gmail.com>
2022-11-10 13:42:55 +0900

    포스트 수정 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [     #IP주소/blog/
     path('', views.PostList.as_view()), # CBV스타일
     path('<int:pk>/', views.PostDetail.as_view()),
-    path('create_post/', views.PostCreate.as_view()),
+    path('create_post/', views.PostCreate.as_view()),   # IP주소/blog/create_post/
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('category/<str:slug>/', views.category_page),  # IP주소/blog/category/slug/
     path('tag/<str:slug>/', views.tag_page),            # IP주소/blog/tag/slug/
     # path('', views.index),   #IP주소/blog  FBV 스타일
